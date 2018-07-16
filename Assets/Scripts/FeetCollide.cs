676bc211d201dbb0308898c2daba08762d40552d
Turtana <eliasvahis@luukku.com>
2018-07-16 23:11:53 +0300

    some files

---
+++ Assets/Scripts/FeetCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetCollide : MonoBehaviour {
+	public PlayerScript playerScript;
+	public GameObject player;
+
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.tag == "Ground") {
+			playerScript.feetOnTheGround();
+		}
+	}
+}
